6f7b6267efc4886c9e8b765200ae92c794e08b0e
Thanakrit Kunchungtong <108317115+tor2545@users.noreply.github.com>
2024-05-03 09:52:57 +0700

    .

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,17 +5,14 @@ class AdminsController < ApplicationController
 
     if @admin.save
       render json: @admin, status: :created, location: @admin
+    
     else
       render json: @admin.errors, status: :unprocessable_entity
     end
   end
 
-  private
-
-  def admin_params
-    params.require(:admin).permit(:username, :password)
-  end
-end
+  
+ 
   # GET /admins
   # GET /admins.json
   def index
@@ -25,26 +22,12 @@ end
   # GET /admins/1
   # GET /admins/1.json
   def show
+    
   end
 
-  # POST /admins
-  # POST /admins.json
-  def self.encrypt(p)
-    Digest::SHA512.hexdigest("=#{@33!}#{p}#{dfgdgf}")
-  end
-  # PATCH/PUT /admins/1
-  # PATCH/PUT /admins/1.json
-  def encrypt(p)
-    self.class.encrypt(p)
-  end
-  # DELETE /admins/1
-  # DELETE /admins/1.json
-  def verify
-    self.user_crypted.user_crypted == encrypt(p) if self.user_crypted
-  end
+  private
 
-  def encrypt_pin_code
-    if is_update.blank?
-      self.pin_code = encrypt(self.pin_code)
-    end
+  def admin_params
+    params.require(:admin).permit(:username, :password)
   end
+end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
-    has_many :token
+    has_many :tokens
 
     def password=(p)
         @password = p
@@ -10,6 +10,21 @@ class Admin < ApplicationRecord
     end
 
     def self.encrypt(p)
-        Digest::SHA512.hexdigest("=#{'dsad'}#{p}#{'dsadasss'}")
+        salt = "papa007x?2#Lnwmakmak"
+        Digest::SHA512.hexdigest("=#{salt}#{p}#{salt}")
     end
+
+    def encrypt(p)
+        self.class.encrypt(p)
+    end
+
+  def verify
+    self.user_crypted.user_crypted == encrypt(p) if self.user_crypted
+  end
+
+  def encrypt_pin_code
+    if is_update.blank?
+      self.pin_code = encrypt(self.pin_code)
+    end
+  end
 end
