234f4280a6bb831fcf9bc72c5549b909b1b0965d
Thanakrit Kunchungtong <thanakrit.ku@kkumail.com>
2024-05-21 14:24:08 +0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.action_cable.url = 'wss://api.training.riseplus.tech/cable'
-  config.action_cable.allowed_request_origins = ['*']
+  config.action_cable.allowed_request_origins = ['https://loans-pam-what-aquarium.trycloudflare.com']
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
